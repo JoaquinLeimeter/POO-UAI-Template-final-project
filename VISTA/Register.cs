99d7e6e05be2447d333439d753eb759189c39bc0
Joaquin leimeter <leimeter.joaquin@gmail.com>
2022-06-20 22:26:02 -0300

    Login WIP

---
+++ VISTA/Register.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VISTA
+{
+    public partial class Register : Form
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            //validar inputs (podría ser en text change, depende de ustedes)
+
+            //try 
+
+                //crear user
+
+                //abrir app y guardar usuario en currentUser (falta crearlo  en controladora usuario)
+
+            //catch error
+        }
+    }
+}
